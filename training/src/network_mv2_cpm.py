12db89c17dfb2fbfdfc5567386c7854e987157f0
edvard <edvard_hua@live.com>
2018-05-13 12:41:47 +0800

    update annotation

--- training/src/network_mv2_cpm.py
+++ training/src/network_mv2_cpm.py
@@ -1,8 +1,17 @@
+# Copyright 2018 Zihua Zeng (edvard_hua@live.com)
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===================================================================================
 # -*- coding: utf-8 -*-
-# @Time    : 18-4-12 5:12 PM
-# @Author  : edvard_hua@live.com
-# @FileName: network_mv2_cpm.py
-# @Software: PyCharm
 
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
