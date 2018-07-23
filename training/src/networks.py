93f4b94417f6e188c12df9e1fd6981dbbada984e
zihua-zeng <edvard_hua@live.com>
2018-07-23 20:16:17 +0800

    update networks.py

--- training/src/networks.py
+++ training/src/networks.py
@@ -5,8 +5,11 @@
 # @Software: PyCharm
 
 import network_mv2_cpm
+import network_mv2_hourglass
 
 def get_network(type, input, trainable=True):
     if type == 'mv2_cpm':
         net, loss = network_mv2_cpm.build_network(input, trainable)
+    elif type == "mv2_hourglass":
+        net, loss = network_mv2_hourglass.build_network(input, trainable)        
     return net, loss
