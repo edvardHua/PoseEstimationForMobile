ee5546a0a6462385ae4442c7341385cce7308102
sampain <pais2301@usherbrooke.ca>
2020-10-11 23:16:18 -0400

    Remove stitch

--- EPMUSMobile/app/src/main/java/com/epmus/mobile/data/LoginDataSource.kt
+++ EPMUSMobile/app/src/main/java/com/epmus/mobile/data/LoginDataSource.kt
@@ -1,7 +1,6 @@
 package com.epmus.mobile.data
 
 import com.epmus.mobile.data.model.LoggedInUser
-import com.epmus.mobile.ui.login.MongoDBAccess
 import java.io.IOException
 
 /**
@@ -12,8 +11,6 @@ class LoginDataSource {
     fun login(username: String, password: String): Result<LoggedInUser> {
         try {
             // TODO: handle loggedInUser authentication
-            val mongoDB = MongoDBAccess()
-            mongoDB.validateUserPassword(username, password)
             val fakeUser = LoggedInUser(java.util.UUID.randomUUID().toString(), "Jane Doe")
             return Result.Success(fakeUser)
         } catch (e: Throwable) {
